f4c2c7c35ed950ff10483a298c41c9cc105014f3
Xero kun <elo48858@gmail.com>
2023-09-14 21:32:45 +0100

    `FEAT` Implemented guessing feature

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guessing Game");
     println!("Please input your number");
 
     let mut guess = String::new();
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Cannot parse string, please input a number");
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Equal => println!("You guessed right!"),
+        Ordering::Greater => println!("Too high!")
+    }
 
     println!("Your guess: {guess}");
 }
